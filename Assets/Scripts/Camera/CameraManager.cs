38e056cb99e9f444ecc266175572a974930c6552
Victoria <viktoria.zheneral@gmail.com>
2022-09-01 16:34:07 +0200

    camera manager script renamed

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwitchCamera : MonoBehaviour
+public class CameraManager : MonoBehaviour
 {
     private Animator animator;
 
