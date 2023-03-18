b71e89f7a86e0732afe860fe3e0eba22467e0d2d
Vykula <viktoria.zheneral@gmail.com>
2023-03-18 20:48:16 +0200

    some logic for time manager
    currently broken a bit

--- Assets/Scripts/Time/TimeManager.cs
+++ Assets/Scripts/Time/TimeManager.cs
@@ -1,23 +1,45 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
     [SerializeField]
-    private Gradient color;
+    private RawImage viewImage;
 
-    void Start()
+    [SerializeField]
+    private int minBrightness;
+    [SerializeField]
+    private int maxBrightness;
+
+    void Start() => StartCoroutine(UpdateTime());
+
+    void OnDestroy() => StopAllCoroutines();
+
+    private IEnumerator UpdateTime()
     {
-        Camera.main.backgroundColor = color.Evaluate(TimePassed());
+        while (true)
+        {
+            viewImage.color = Color.HSVToRGB(0, 0, TimePassed() * 100);
+
+            //yield return new WaitForSecondsRealtime(300);
+            yield return new WaitForEndOfFrame();
+        }
     }
 
+    /// <summary>Calculates time passed since midnight in seconds.</summary>
+    /// <returns>Returns time ratio (from 0 to 1).</returns>
     private float TimePassed()
     {
         DateTime time = DateTime.Now;
 
-        float passed = time.Hour * 60 * 60 + time.Minute * 60 + time.Second;
-        float full = 24 * 60 * 60;
+        float secondsPassed = time.Hour * 60 * 60 + time.Minute * 60 + time.Second;
+        float secondsFull = 24 * 60 * 60;
 
-        return passed / full;
+        float timeRatio = secondsPassed / secondsFull;
+
+        //TODO: Should return ratio as for timelapse. Currently logic is broken.
+        return timeRatio < 0.5 ? timeRatio : 1 - timeRatio;
     }
 }
