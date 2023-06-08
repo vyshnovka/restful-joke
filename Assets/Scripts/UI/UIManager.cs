c04f0ef2c12fb6e69b3e773fbad631fa81743e6e
Vykula <viktoria.zheneral@gmail.com>
2023-06-08 22:32:10 +0300

    gameplay transition added
    need a way to open settings and quit the game

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -2,5 +2,20 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject gameplayCanvas;
+    [SerializeField]
+    private GameObject loadingGif;
+    [SerializeField]
+    private GameObject windowsBackground;
+
+    public void StartGameplay()
+    {
+        // Not the nicest solution, need to find more robust way.
+        StartCoroutine(gameplayCanvas.ToggleActiveAfterDelay(4.5f));
+        StartCoroutine(loadingGif.ToggleActiveAfterDelay(10));
+        StartCoroutine(windowsBackground.ToggleActiveAfterDelay(10));
+    }
+
     public void ExitGame() => Application.Quit();
 }
