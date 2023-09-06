9acb755612ac2141d419a8d379e6d62b01f86000
Vykula <viktoria.zheneral@gmail.com>
2023-09-06 18:55:52 +0300

    formatting

--- Assets/Scripts/API/JokeAPI.cs
+++ Assets/Scripts/API/JokeAPI.cs
@@ -10,7 +10,7 @@ public class Joke
     public string category;
     public string type;
     public string joke = "There are no jokes for you today. But hasn't your life laughed at you yet?"; // Default value in case of missing joke (e.g. an error in web response might lead to this field being empty).
-    public struct flags
+    public struct Flags
     {
         public bool nsfw;
         public bool religious;
